4d78b63849164c64cef52984636968ab0ed1b98b
CoderVinii <viniciusdmfernandes@gmail.com>
2026-04-17 17:06:36 -0300

    Atualização base

--- app/presente/[id]/page.js
+++ app/presente/[id]/page.js
@@ -6,9 +6,9 @@ export default async function Presente({ params, searchParams }) {
   const { id } = params
 
   const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-  )
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
+)
 
   const { data: presente, error } = await supabase
     .from('presentes')
